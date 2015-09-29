6e7088624ca21becf1149be800bf67b2773a4a4c
Gibran Arias <gibran.arias@mexicoder.com.mx>
2015-09-29 14:02:05 -0500

    Fourth step of tutorial

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls import include
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('snippets.urls')),
+]
+
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
